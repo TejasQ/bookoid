4954dcc93cc7d44b3ad490531fbc14d3725a8b8f
Tejas Kumar <tejas@tejas.qa>
2020-09-17 12:17:50 +0200

    Use vh instead of % for container height

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import React, { FC } from 'react'
 import '../css/tailwind.css'
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
-    return <main className="min-h-full bg-gray-400"><Component {...pageProps} /></main>
+    return <main className="min-h-screen bg-gray-400"><Component {...pageProps} /></main>
 }
 
 export default MyApp
