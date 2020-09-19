852f0cb4db93edadd5455fded68b1948e80a43a6
Pavithran Gridharan <pavethiran@gmail.com>
2020-09-19 12:16:06 +0200

    fix: remove unused props argument

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,15 +1,15 @@
-import * as React from "react";
+import * as React from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface BookCardProps {}
 
-const BookCard: React.FC<BookCardProps> = (props) => {
+const BookCard: React.FC<BookCardProps> = () => {
   return (
     <div className="w-40 m-2">
       <img
         className="overflow-hidden rounded-lg shadow-lg mb-2"
         src={`https://loremflickr.com/280/400?${(Math.random() * 100).toFixed(
-          0
+          0,
         )}`}
         alt="Sunset in the mountains"
       />
