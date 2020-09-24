b3fd5a5e7021ba18ce888ab96921b664ca6bd366
Pavithran Gridharan <pavethiran@gmail.com>
2020-09-24 19:32:41 +0200

    refactor: extract icons into components

---
+++ components/ViewListIcon.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+// https://github.com/tailwindlabs/heroicons/blob/master/src/solid/view-list.svg
+const ViewListIcon: React.FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M4 6h16M4 10h16M4 14h16M4 18h16"
+    />
+  </svg>
+);
+
+export default ViewListIcon;
--- components/ViewToggle.tsx
+++ components/ViewToggle.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import ViewGridIcon from './ViewGridIcon';
+import ViewListIcon from './ViewListIcon';
 
 type ViewToggleProps = {
   onClick?: (
@@ -39,36 +41,10 @@ const ViewToggle: React.FC<ViewToggleProps> = (props) => {
       onClick={onClickHandler}
     >
       <div className={`rounded p-1${listStyle}`}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M4 6h16M4 10h16M4 14h16M4 18h16"
-          />
-        </svg>
+        <ViewListIcon />
       </div>
       <div className={`rounded p-1${gridStyle}`}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
-          />
-        </svg>
+        <ViewGridIcon />
       </div>
     </div>
   );
