88fcc0056064fc5dd9fb8ee00e93cecd65f496ac
Pavithran Gridharan <pavethiran@gmail.com>
2020-09-24 19:46:39 +0200

    refactor: lift ViewToggle state up

--- components/ViewToggle.tsx
+++ components/ViewToggle.tsx
@@ -5,45 +5,31 @@ import clsx from 'clsx';
 import ViewGridIcon from './ViewGridIcon';
 import ViewListIcon from './ViewListIcon';
 
+export type ViewToggleValue = 'grid' | 'list';
+
 type ViewToggleProps = {
-  onClick?: (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-    state: 'grid' | 'list',
-  ) => void;
-  value?: 'grid' | 'list';
+  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  value?: ViewToggleValue;
 };
 
 const ViewToggle: React.FC<ViewToggleProps> = (props) => {
   const { onClick, value } = props;
 
-  const [state, setState] = React.useState(value ?? 'grid');
-
-  const onClickHandler = React.useCallback(
-    (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-      const newState = state === 'grid' ? 'list' : 'grid';
-      setState(newState);
-      if (onClick) {
-        onClick(event, newState);
-      }
-    },
-    [state, setState, onClick],
-  );
-
   return (
     <div
       className="flex rounded p-1 cursor-pointer bg-gray-500"
-      onClick={onClickHandler}
+      onClick={onClick}
     >
       <div
         className={clsx('rounded', 'p-1', {
-          'shadow bg-white': state === 'list',
+          'shadow bg-white': value === 'list',
         })}
       >
         <ViewListIcon />
       </div>
       <div
         className={clsx('rounded', 'p-1', {
-          'shadow bg-white': state !== 'list',
+          'shadow bg-white': value !== 'list',
         })}
       >
         <ViewGridIcon />
