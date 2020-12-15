9f95841c7124c9da8df29b60af22c766ef06101a
Siddharth Kundu <sk31395@gmail.com>
2020-12-15 09:44:16 +0530

    chore:updated dependencies to use latest stable release

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
-module.exports = {};
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
